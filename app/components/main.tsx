e5970fdd06265efce588e6482b87350707813ace
shunakid <shunakid@icloud.com>
2023-08-16 23:34:58 +0900

    キャラクターを選択した時に、自己紹介の音声を再生されることを確認

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -4,13 +4,39 @@ import { useRef, useState } from "react";
 import { CharacterType } from "./types";
 import { Characters } from "./config";
 import CharacterSelect from "./character-select";
+import axios from "axios";
 
 // メインコンポーネント
 const Main = () => {
   const [character, setCharacter] = useState<CharacterType>(Characters[0]);
 
   // 音声再生
-  const playAudio = async (text: string, speaker: string) => {};
+  const playAudio = async (text: string, speaker: string) => {
+    try {
+      // 音声取得
+      const responseAudio = await axios.post("/api/audio", {
+        text,
+        speaker,
+      });
+
+      // Base64形式で取得
+      const base64Audio = responseAudio?.data?.response;
+      // Bufferに変換
+      const byteArray = Buffer.from(base64Audio, "base64");
+      // Blobに変換
+      const audioBlob = new Blob([byteArray], { type: "audio/x-wav" });
+      // URLに変換
+      const audioUrl = URL.createObjectURL(audioBlob);
+      // 音声作成
+      const audio = new Audio(audioUrl);
+      // 音量[0-1]設定
+      audio.volume = 1;
+      // 再生
+      audio.play();
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
