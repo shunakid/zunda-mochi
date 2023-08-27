c049e826c9177f2c151da3fbf1ec4dd0ed7df059
shunakid <shunakid@icloud.com>
2023-08-27 12:13:37 +0900

    supabase連携

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navigation from "./components/navigation";
+import SupabaseProvider from "./components/supabase-provider";
 
 export const metadata = {
   title: "ずんだもち",
@@ -14,15 +15,17 @@ export default function RootLayout({
   return (
     <html>
       <body>
-        <div className="flex flex-col min-h-screen relative bg-slate-800">
-          <Navigation />
-          <div className="h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900 text-gray-500">
-            sidebar
+        <SupabaseProvider>
+          <div className="flex flex-col min-h-screen relative bg-slate-800">
+            <Navigation />
+            <div className="h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900 text-gray-500">
+              sidebar
+            </div>
+            <main className="flex-1 container max-w-screen-sm mx-auto px-5 py-5">
+              {children}
+            </main>
           </div>
-          <main className="flex-1 container max-w-screen-sm mx-auto px-5 py-5">
-            {children}
-          </main>
-        </div>
+        </SupabaseProvider>
       </body>
     </html>
   );
